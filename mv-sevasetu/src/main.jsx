2c80d2ce7f4e5a04cdf823a5beae772f1fdce0ef
bongo <287613933+bongolabs@users.noreply.github.com>
2026-08-06 18:19:32 +0000

    fix(core): add eslint bypass for context export to resolve fast refresh build failure

--- mv-sevasetu/src/main.jsx
+++ mv-sevasetu/src/main.jsx
@@ -53,6 +53,7 @@ const SystemTranslations = {
 };
 
 // GLOBAL CONTEXT INITIALIZATION
+// eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
 
 function AppProvider({ children }) {
